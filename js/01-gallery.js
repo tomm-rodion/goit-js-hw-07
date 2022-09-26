07d33c9da794afb33bfb8d85291c88a287e4f70b
tomm-rodion <autovolna@gmail.com>
2022-09-26 19:49:48 +0300

    hw-07 (1)

---
+++ js/01-gallery.js
@@ -0,0 +1,47 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+const gallery = document.querySelector(".gallery");
+
+gallery.insertAdjacentHTML("beforeend", createGalleryItemsMarkup(galleryItems));
+gallery.addEventListener("click", onOpenModalImg);
+
+function createGalleryItemsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+
+function onOpenModalImg(evt) {
+  evt.preventDefault();
+  const isImg = evt.target.classList.contains(".gallery__link");
+  if (isImg) {
+    return;
+  }
+  const imgTargetGallery = evt.target.dataset.source;
+  const instance = basicLightbox.create(
+    `<div class="modal">
+        <img src = "${imgTargetGallery}" width="800" height="600">/>
+    </div>`
+  );
+
+  instance.show();
+
+  if (instance.visible()) {
+    gallery.addEventListener("keydown", (evt) => {
+      if (evt.code === "Escape") {
+        instance.close();
+      }
+    });
+  }
+}

c2da3574bab14b5b2a49ff7299476d5dd446c660
tomm-rodion <autovolna@gmail.com>
2023-04-13 23:37:11 +0300

    arrey products internet shop

---
+++ js/03-internet-shop.js
@@ -0,0 +1,26 @@
+const phone = [
+  {
+    id: 1,
+    img: "https://www.apple.com/v/macbook-pro-14-and-16/b/images/meta/macbook-pro-14-and-16_overview__fz0lron5xyuu_og.png",
+    name: "MacBooc Pro",
+    price: 2800,
+  },
+  {
+    id: 1,
+    img: "https://i.citrus.world/imgcache/size_800/uploads/shop/6/7/6737e169ab2b08ee71f5910666b43b40.jpg",
+    name: "iPhone 14 Pro",
+    price: 1300,
+  },
+  {
+    id: 2,
+    img: "https://cdn1.it4profit.com/AfrOrF3gWeDA6VOlDG4TzxMv39O7MXnF4CXpKUwGqRM/resize:fill:540/bg:f6f6f6/q:100/plain/s3://catalog-products/220908083559646887/220908140013965815.png@webp",
+    name: "AirPods Pro2",
+    price: 400,
+  },
+  {
+    id: 3,
+    img: "https://www.apple.com/newsroom/images/product/watch/standard/Apple_watch-experience-for-entire-family-hero_09152020_big.jpg.large.jpg",
+    name: "iPhone 14 Pro",
+    price: 600,
+  },
+];
